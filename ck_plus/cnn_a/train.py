0bb232461153213d4e883d03a9781e927b11b34d
pkhorrami4 <pkhorrami4@gmail.com>
2017-03-12 09:47:34 -0500

    Add new train scripts with dropout and data augmentation.

--- ck_plus/cnn_a/train.py
+++ ck_plus/cnn_a/train.py
@@ -7,8 +7,9 @@ import numpy
 
 from anna import util
 from anna.datasets import supervised_dataset
-from anna.datasets.supervised_data_loader import SupervisedDataLoaderCrossVal
+#from anna.datasets.supervised_data_loader import SupervisedDataLoaderCrossVal
 
+import data_fold_loader
 import data_paths
 from model import SupervisedModel
 
@@ -17,52 +18,66 @@ parser = argparse.ArgumentParser(prog='train_cnn_with_data_augmentation',
                                  description='Script to train convolutional \
                                  neural network from random initialization \
                                  with data augmentation.')
-parser.add_argument("-s", "--split", default='0', help='Training split of CK+ \
+parser.add_argument("-s", "--split", default='0', help='Testing split of CK+ \
                     to use. (0-9)')
+parser.add_argument("--checkpoint_dir", default='./', help='Location to save \
+                    model checkpoint files.')
 args = parser.parse_args()
 
 print('Start')
-train_split = int(args.split)
-if train_split < 0 or train_split > 9:
-    raise Exception("Training Split must be in range 0-9.")
-print('Using CK+ training split: {}'.format(train_split))
+test_split = int(args.split)
+if test_split < 0 or test_split > 9:
+    raise Exception("Testing Split must be in range 0-9.")
+print('Using CK+ testing split: {}'.format(test_split))
+
+checkpoint_dir = os.path.join(args.checkpoint_dir, 'checkpoints_'+str(test_split))
+print 'Checkpoint dir: ', checkpoint_dir
 
 pid = os.getpid()
 print('PID: {}'.format(pid))
-f = open('pid_'+str(train_split), 'wb')
+f = open('pid_'+str(test_split), 'wb')
 f.write(str(pid)+'\n')
 f.close()
 
 # Load model
 model = SupervisedModel('experiment', './', learning_rate=1e-2)
 monitor = util.Monitor(model,
-                       checkpoint_directory='checkpoints_'+str(train_split),
+                       checkpoint_directory=checkpoint_dir,
                        save_steps=1000)
 
 # Loading CK+ dataset
 print('Loading Data')
-supervised_data_loader = SupervisedDataLoaderCrossVal(
-    data_paths.ck_plus_data_path)
-train_data_container = supervised_data_loader.load('train', train_split)
-test_data_container = supervised_data_loader.load('test', train_split)
+#supervised_data_loader = SupervisedDataLoaderCrossVal(
+#    data_paths.ck_plus_data_path)
+#train_data_container = supervised_data_loader.load('train', train_split)
+#test_data_container = supervised_data_loader.load('test', train_split)
 
-X_train = train_data_container.X
+train_folds, val_fold, _ = data_fold_loader.load_fold_assignment(test_fold=test_split)
+X_train, y_train = data_fold_loader.load_folds(data_paths.ck_plus_data_path, train_folds)
+X_val, y_val = data_fold_loader.load_folds(data_paths.ck_plus_data_path, [val_fold])
+X_test, y_test = data_fold_loader.load_folds(data_paths.ck_plus_data_path, [test_split])
+
+print X_train.shape, y_train.shape
+print X_val.shape, y_val.shape
+print X_test.shape, y_test.shape
+
 X_train = numpy.float32(X_train)
 X_train /= 255.0
 X_train *= 2.0
-y_train = train_data_container.y
 
-X_test = test_data_container.X
+X_val = numpy.float32(X_val)
+X_val /= 255.0
+X_val *= 2.0
+
 X_test = numpy.float32(X_test)
 X_test /= 255.0
 X_test *= 2.0
-y_test = test_data_container.y
 
 train_dataset = supervised_dataset.SupervisedDataset(X_train, y_train)
-test_dataset = supervised_dataset.SupervisedDataset(X_test, y_test)
+val_dataset = supervised_dataset.SupervisedDataset(X_val, y_val)
 train_iterator = train_dataset.iterator(
     mode='random_uniform', batch_size=64, num_batches=31000)
-test_iterator = test_dataset.iterator(
+val_iterator = val_dataset.iterator(
     mode='random_uniform', batch_size=64, num_batches=31000)
 
 # Do data augmentation (crops, flips, rotations, scales, intensity)
@@ -70,9 +85,9 @@ data_augmenter = util.DataAugmenter2(crop_shape=(96, 96),
                                      flip=True, gray_on=True)
 normer = util.Normer3(filter_size=5, num_channels=1)
 module_list_train = [data_augmenter, normer]
-module_list_test = [normer]
+module_list_val = [normer]
 preprocessor_train = util.Preprocessor(module_list_train)
-preprocessor_test = util.Preprocessor(module_list_test)
+preprocessor_val = util.Preprocessor(module_list_val)
 
 print('Training Model')
 for x_batch, y_batch in train_iterator:
@@ -83,7 +98,7 @@ for x_batch, y_batch in train_iterator:
 
     if monitor.test:
         monitor.start()
-        x_test_batch, y_test_batch = test_iterator.next()
-        x_test_batch = preprocessor_test.run(x_test_batch)
-        test_accuracy = model.eval(x_test_batch, y_test_batch)
-        monitor.stop_test(1-test_accuracy)
+        x_val_batch, y_val_batch = val_iterator.next()
+        x_val_batch = preprocessor_val.run(x_val_batch)
+        val_accuracy = model.eval(x_val_batch, y_val_batch)
+        monitor.stop_test(1-val_accuracy)
--- ck_plus/cnn_d/train.py
+++ ck_plus/cnn_d/train.py
@@ -7,8 +7,9 @@ import numpy
 
 from anna import util
 from anna.datasets import supervised_dataset
-from anna.datasets.supervised_data_loader import SupervisedDataLoaderCrossVal
+#from anna.datasets.supervised_data_loader import SupervisedDataLoaderCrossVal
 
+import data_fold_loader
 import data_paths
 from model import SupervisedModel
 
@@ -17,26 +18,31 @@ parser = argparse.ArgumentParser(prog='train_cnn_with_dropout',
                                  description='Script to train convolutional \
                                  neural network from random initialization \
                                  with dropout.')
-parser.add_argument("-s", "--split", default='0', help='Training split of CK+ \
+parser.add_argument("-s", "--split", default='0', help='Testing split of CK+ \
                     to use. (0-9)')
+parser.add_argument("--checkpoint_dir", default='./', help='Location to save \
+                    model checkpoint files.')
 args = parser.parse_args()
 
 print('Start')
-train_split = int(args.split)
-if train_split < 0 or train_split > 9:
-    raise Exception("Training Split must be in range 0-9.")
-print('Using CK+ training split: {}'.format(train_split))
+test_split = int(args.split)
+if test_split < 0 or test_split > 9:
+    raise Exception("Testing Split must be in range 0-9.")
+print('Using CK+ testing split: {}'.format(test_split))
+
+checkpoint_dir = os.path.join(args.checkpoint_dir, 'checkpoints_'+str(test_split))
+print 'Checkpoint dir: ', checkpoint_dir
 
 pid = os.getpid()
 print('PID: {}'.format(pid))
-f = open('pid_'+str(train_split), 'wb')
+f = open('pid_'+str(test_split), 'wb')
 f.write(str(pid)+'\n')
 f.close()
 
 # Load model
 model = SupervisedModel('experiment', './', learning_rate=1e-2)
 monitor = util.Monitor(model,
-                       checkpoint_directory='checkpoints_'+str(train_split),
+                       checkpoint_directory=checkpoint_dir,
                        save_steps=1000)
 
 # Add dropout to fully-connected layer
@@ -45,28 +51,36 @@ model._compile()
 
 # Loading CK+ dataset
 print('Loading Data')
-supervised_data_loader = SupervisedDataLoaderCrossVal(
-    data_paths.ck_plus_data_path)
-train_data_container = supervised_data_loader.load('train', train_split)
-test_data_container = supervised_data_loader.load('test', train_split)
+#supervised_data_loader = SupervisedDataLoaderCrossVal(
+#    data_paths.ck_plus_data_path)
+#train_data_container = supervised_data_loader.load('train', train_split)
+#test_data_container = supervised_data_loader.load('test', train_split)
+train_folds, val_fold, _ = data_fold_loader.load_fold_assignment(test_fold=test_split)
+X_train, y_train = data_fold_loader.load_folds(data_paths.ck_plus_data_path, train_folds)
+X_val, y_val = data_fold_loader.load_folds(data_paths.ck_plus_data_path, [val_fold])
+X_test, y_test = data_fold_loader.load_folds(data_paths.ck_plus_data_path, [test_split])
 
-X_train = train_data_container.X
+print X_train.shape, y_train.shape
+print X_val.shape, y_val.shape
+print X_test.shape, y_test.shape
+
 X_train = numpy.float32(X_train)
 X_train /= 255.0
 X_train *= 2.0
-y_train = train_data_container.y
 
-X_test = test_data_container.X
+X_val = numpy.float32(X_val)
+X_val /= 255.0
+X_val *= 2.0
+
 X_test = numpy.float32(X_test)
 X_test /= 255.0
 X_test *= 2.0
-y_test = test_data_container.y
 
 train_dataset = supervised_dataset.SupervisedDataset(X_train, y_train)
-test_dataset = supervised_dataset.SupervisedDataset(X_test, y_test)
+val_dataset = supervised_dataset.SupervisedDataset(X_val, y_val)
 train_iterator = train_dataset.iterator(
     mode='random_uniform', batch_size=64, num_batches=31000)
-test_iterator = test_dataset.iterator(
+val_iterator = val_dataset.iterator(
     mode='random_uniform', batch_size=64, num_batches=31000)
 
 # Create object to local contrast normalize a batch.
@@ -84,7 +98,7 @@ for x_batch, y_batch in train_iterator:
 
     if monitor.test:
         monitor.start()
-        x_test_batch, y_test_batch = test_iterator.next()
-        x_test_batch = preprocessor.run(x_test_batch)
-        test_accuracy = model.eval(x_test_batch, y_test_batch)
-        monitor.stop_test(1-test_accuracy)
+        x_val_batch, y_val_batch = val_iterator.next()
+        x_val_batch = preprocessor.run(x_val_batch)
+        val_accuracy = model.eval(x_val_batch, y_val_batch)
+        monitor.stop_test(1-val_accuracy)
